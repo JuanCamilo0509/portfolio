d8fb9670af6ac038a53804fd5252fb3eb8777b32
camilo <juank109845@gmail.com>
2024-06-06 03:56:55 -0500

    Adding upperCase to the titles in the skills description section

--- src/modules/description_skills.jsx
+++ src/modules/description_skills.jsx
@@ -3,11 +3,12 @@ import '../styles/skills.css';
 function Description({ text, data, paint }) {
 	const value = data[text] ? data[text][1] : "Hover an item";
 	const fillcolor = `rgb(${paint.join(', ')})`;
+	const title = text.charAt(0).toUpperCase() + text.slice(1);
 
 	return (
 		<>
 			<div className="skill_description">
-				<h2 style={{ color: fillcolor }}>{text}</h2>
+				<h2 style={{ color: fillcolor }}>{title}</h2>
 				<p>{value}</p>
 			</div>
 		</>
